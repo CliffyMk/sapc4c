653f75d24a30798ce5dd4c5819fd3789f32ea631
manish kumar <man90.4u@gmail.com>
2016-07-08 16:16:46 +0530

    final working drag and drop demo

--- demo1/app/app.directive.js
+++ demo1/app/app.directive.js
@@ -2,36 +2,14 @@
 (function () {
     'use strict';
     var app = angular.module('MappingApp');
-    var srcDragElement = null;
-    var innerHTML = null;
     app.directive('draggable', function () {
         return {
             restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
             link: function ($scope, element, attr, controller) {
-                // element.bind('dragstart', function (e) {
-                //     console.log("drag event occured");
-                //     srcDragElement = e.target;
-                //     innerHTML = srcDragElement.innerHTML;
-                //     //e.dataTransfer.setData("text/html", srcDragElement.innerHTML);
-                // });
-                // element.bind('dragenter', function (e) {
-                //     e.stopPropagation();
-                //     e.preventDefault();
-
-                // });
-                // element.bind('dragleave', function (e) {
-                //     e.stopPropagation();
-                //     e.preventDefault();
-
-                // });
-                // element.bind('drop', function (e) {
-                //     console.log("drop funciotn called");
-                //     e.stopPropagation();
-                //     e.preventDefault();
-                //     console.log("drop funciotn called");
-                // });
-                console.log(element);
                 element[0].addEventListener('dragstart', $scope.handleDragEvent, false);
+                element[0].addEventListener('dragend', $scope.handleDragEnd, false);
+                element[0].addEventListener('dragover', $scope.handleDragOver, false);
+                element[0].addEventListener('drop', $scope.handleDropEvent, false);
             }
         };
     });
@@ -39,10 +17,15 @@
         // Runs during compile
         return {
             restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-            link: function ($scope, element, attr, controller) {
-                console.log("drop has been initialised");
-                element[0].addEventListener('drop', $scope.handleDropEvent, false);
+            link: function (vm, element, attr, controller) {
+                element[0].addEventListener('drop', vm.handleDropEvent, false);
+                // element.bind("drop", function (event) {
+                //     /* Act on the event */
+                //     event.preventDefault();
+                //     console.log("drop called");
+                // });
             }
         };
     });
+
 }());
--- demo1/app/components/mapping/mappingCtrl.js
+++ demo1/app/components/mapping/mappingCtrl.js
@@ -1,84 +1,81 @@
 (function () {
     'use strict';
     angular.module('MappingApp')
-        .controller('MappingController', ['$scope', function ($scope) {
-            var vm = this;
-            var srcDragElement = null;
+        .controller('MappingController', ['$scope', 'Excel', '$timeout', function ($scope, Excel, $timeout) {
             console.log("mapping contrller loaded");
-            vm.getLeftColumn = function () {
-                vm.leftColumn = [];
-                console.log('i am clicked');
-                var fileInput = document.getElementById("csv"),
-                    readFile = function () {
-                        var reader = new FileReader();
-                        reader.onload = function () {
-                            console.log(data);
-                            var data = reader.result.split('/\r\n|\n/');
-                            console.log(data);
-                            data = data[0].split('\n');
-                            console.log(data);
-                            data = data[0].split(',');
-                            $.each(data, function (index, val) {
-                                vm.leftColumn.push(val);
-                            });
-                            $scope.$apply();
+            var vm = this;
+            vm.srcDragElement = {};
+            vm.rightColumn = ["row1", "row2", "row3", "row4"];
+            var leftFileInput = document.getElementById("leftCsv");
+            var readLeftFile = function () {
+                if (readLeftFile !== null) {
+                    vm.leftColumn = [];
+                    var reader = new FileReader();
+                    reader.onload = function () {
+                        var data = reader.result.split('/\r\n|\n/');
+                        data = data[0].split('\n');
+                        data = data[0].split(',');
+                        $.each(data, function (index, val) {
+                            if (val.replace(" ", "") == "") return true;
+                            vm.leftColumn.push(val);
+                        });
 
-                        };
-                        reader.readAsText(fileInput.files[0]);
-                        reader.onload();
-                        // start reading the file. When it is done, calls the onload event defined above.
+                        vm.leftColumnObject = vm.leftColumn[0];
+                        console.log("vm.leftColumn[0]" + vm.leftColumn[0]);
+                        vm.leftColumn.shift();
+                        // vm.leftColumn.pop();
+                        vm.mappedColumn = vm.leftColumn;
 
-                    };
-                fileInput.addEventListener('change', readFile);
-            }
-            vm.getRightColumn = function () {
-                vm.rightColumn = [];
-                console.log('i am clicked');
-                var fileInput = document.getElementById("csv2"),
-                    readFile = function () {
-                        var reader = new FileReader();
-                        reader.onload = function () {
-                            console.log(data);
-                            var data = reader.result.split('/\r\n|\n/');
-                            console.log(data);
-                            data = data[0].split('\n');
-                            console.log(data);
-                            data = data[0].split(',');
-                            $.each(data, function (index, val) {
-                                vm.rightColumn.push(val);
-                            });
-                            $scope.$apply();
+                        $scope.$apply();
 
-                        };
-                        reader.readAsText(fileInput.files[0]);
-                        reader.onload();
-                        // start reading the file. When it is done, calls the onload event defined above.
 
                     };
-                fileInput.addEventListener('change', readFile);
-            }
-
-            $scope.handleDragEvent = function (ev) {
-                console.log("drag event occured");
-                srcDragElement = ev.target;
-                ev.dataTransfer.setData("text/html", srcDragElement.innerHTML);
-            }
-
-            $scope.handleDropEvent = function (ev) {
-                console.log("drop event occ");
-                ev.preventDefault();
-                ev.stopPropagation();
-                if (srcDragElement != this) {
-                    // $scope.$apply(function () {
-                    //     srcDragElement = ev.target.innerHTML;
-                    //     ev.target.innerHTML = ev.dataTransfer.getData("text/html");
-                    // });
-
+                    reader.readAsText(leftFileInput.files[0]);
+                    reader.onload();
                 }
+
+            };
+            leftFileInput.addEventListener('change', readLeftFile);
+            // reading and displaying the right csv col
+            var rightFileInput = document.getElementById("rightCsv");
+            var readRightFile = function () {
+                console.log("i am in  right file ffn ");
+                vm.rightColumn = [];
+                var reader = new FileReader();
+                reader.onload = function () {
+                    var data = reader.result.split('/\r\n|\n/');
+                    data = data[0].split('\n');
+                    data = data[0].split(',');
+                    $.each(data, function (index, val) {
+                        vm.rightColumn.push(val);
+                    });
+                    $scope.$apply();
+                };
+                reader.readAsText(rightFileInput.files[0]);
+                reader.onload();
+                // start reading the file. When it is done, calls the onload event defined above.
+
+            };
+            rightFileInput.addEventListener('change', readRightFile);
+
+            $scope.handleDragEvent = function (event) {
+                $scope.srcDragElement = this;
+                event.dataTransfer.setData("Text/html", this.innerHTML);
+                this.style.opacity = 0.3;
             }
-            $scope.allowDrop = function (ev) {
-                console.log("drop allowed");
-                ev.preventDefault();
+            $scope.handleDragEnd = function (event) {
+                this.style.opacity = 1;
+            }
+            $scope.handleDropEvent = function (event) {
+                $scope.srcDragElement.innerHTML = this.innerHTML;
+                this.innerHTML = event.dataTransfer.getData("text/html");
+            }
+            $scope.handleDragOver = function (event) {
+                event.preventDefault();
+            }
+            $scope.exportToExcel = function (tableId) { // ex: '#my-table'
+                var exportHref = Excel.tableToExcel(tableId, 'sheet name');
+                $timeout(function () { location.href = exportHref; }, 100); // trigger download
             }
         }]);
 })();
